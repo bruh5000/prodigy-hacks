a73ac8531d6faf69d8d4e9c816a878911ecc0a9c
bruh5000 <44099468+bruh5000@users.noreply.github.com>
2020-02-15 15:06:22 -0600

    Create lvl100allpets.js

---
+++ lvl100allpets.js
@@ -0,0 +1,6 @@
+//use after getallpets//
+for (let PetLoopTest = 0; PetLoopTest < PIXI.game.prodigy.player.kennel.data.length; PetLoopTest++) { 
+     PIXI.game.prodigy.player.kennel.data[PetLoopTest].level=100
+    };
+//for bookmarklet//
+javascript:(function()%7Bfor%20(let%20PetLoopTest%20%3D%200%3B%20PetLoopTest%20%3C%20PIXI.game.prodigy.player.kennel.data.length%3B%20PetLoopTest%2B%2B)%20%7B%20%0A%20%20%20%20%20PIXI.game.prodigy.player.kennel.data%5BPetLoopTest%5D.level%3D100%0A%20%20%20%20%7D%3B%7D)()%3B
