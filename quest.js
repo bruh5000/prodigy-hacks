d2421483e309015e2f45f532e3db4d67ab6dcd01
bruh5000 <44099468+bruh5000@users.noreply.github.com>
2020-02-15 15:13:04 -0600

    Create quest.js

---
+++ quest.js
@@ -0,0 +1,20 @@
+//put quest location inbetween quotes//
+PIXI.game.prodigy.debugQuests.completeQuestRequirements("")
+//areas,locations below//
+forest
+shiverchill
+skywatch
+lamplight
+dyno
+bonfire_spire
+house
+toyzone
+shipwreck_shore
+darktower
+academy
+archives
+earthtower
+tower_town
+//for bookmarklet//
+javascript:(function()%7BPIXI.game.prodigy.debugQuests.completeQuestRequirements("")%7D)()%3B
+//location inbetween quotes//
