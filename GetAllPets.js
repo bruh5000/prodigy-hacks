6ae7d8e77a8bcd08a0cb2c98a2661c4abf3db91c
bruh5000 <44099468+bruh5000@users.noreply.github.com>
2020-02-15 20:01:44 -0600

    Update GetAllPets.js

--- GetAllPets.js
+++ GetAllPets.js
@@ -1,3 +1,3 @@
 PIXI.game.prodigy.debugMisc.getAllPets();
-//for bookmarklet use cod below//
+//for bookmarklet use code below//
 javascript:(function()%7BPIXI.game.prodigy.debugMisc.getAllPets()%3B%7D)()%3B
